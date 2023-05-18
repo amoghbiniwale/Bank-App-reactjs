a78584901d4c8b45b47548bc5dc77b46f924bb5a
Amogh Biniwal <amoghbiniwale04@gmail.com>
2023-05-18 12:40:12 +0530

    bank app front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/login/Login';
 import logo from './logo.svg';
 import './App.css';
-
+import AdminDashboard from './components/AdminDashboard/AdminDashboard';
+import AddNewComponent from './components/AddNewComponent/AddNewComponent';
+import UpdateBank from './components/UpdateBank/UpdateBank';
+import Customer from './components/AdminDashboard/Customer/Customer';
+import UpdateCustomer from './components/AdminDashboard/Customer/UpdateCustomer';
+import AddNewCustomer from './components/AdminDashboard/Customer/AddNewCustomer';
+import Accounts from './components/AdminDashboard/Accounts/Accounts';
+import AddNewAccount from './components/AdminDashboard/Accounts/AddNewAccount';
+import UserDashboard from './components/UserDashboard/UserDashboard';
+import Passbook from './components/UserDashboard/Passbook/Passbook';
+import Transaction from './components/UserDashboard/Transaction/Transaction';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path='/admindashboard/:username/:role/:token' element={<AdminDashboard />} />
+          <Route exact path='/addnew/:username/:role/:token' element={<AddNewComponent />} />
+          <Route exact path='/update/:username/:role/:token/:bankId/:name/:abbriviation' element={<UpdateBank />} />
+          <Route exact path='/customer/:username/:role/:token' element={<Customer />} />
+          <Route exact path='/addnewCustomer/:username/:role/:token' element={<AddNewCustomer />} />
+         <Route exact path='/updateCustomer/:username/:role/:token/:userId/:firstName/:lastName/:email' element={<UpdateCustomer />} />
+         <Route exact path='/accounts/:username/:role/:token' element={<Accounts />} />
+         <Route exact path='/addnewAccount/:username/:role/:token' element={<AddNewAccount />} />
+         
+         <Route exact path='/userdashboard/:username/:role/:token/:userId' element={<UserDashboard />} />
+         <Route exact path='/passbook/:username/:role/:token/:userId/:accountNo' element={<Passbook />} />
+         <Route exact path='/transaction/:username/:role/:token/:userId' element={<Transaction />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/UserDashboard/Passbook/Passbook.js
@@ -0,0 +1,93 @@
+// import React from 'react'
+// import { useNavigate,useParams } from 'react-router-dom';
+// import { useState } from 'react';
+// import NavBar from '../../../layouts/NavBar/NavBar';
+// import axios from 'axios';
+// import { useEffect } from 'react';
+
+// const Passbook = () => {
+//     const navigateObject = new useNavigate();
+
+//     const userDetails = {
+//       username: useParams().username,
+//       role: useParams().role,
+//       token: useParams().token,
+//       userId: useParams().userId,
+//       accountNo: useParams().accountNo
+//     };
+  
+//     console.log(userDetails);
+//     const [transactions, setTransactions] = useState([]);
+  
+//     const getAllTransactions = async () => {
+//       try {
+//         let response = await axios.get(
+//           `http://localhost:8080/api/v1/user/transactions/${userDetails.accountNo}`,
+//           {
+//             headers: {
+//               "Content-Type": "application/json",
+//               withCredentials: true,
+//               Authorization: `Bearer ${userDetails.token}`,
+//             },
+//           }
+//         );
+//         console.log(response.data);
+//         setTransactions(response.data);
+//         console.log(transactions);
+//       } catch (error) {
+//         alert("Error No Data Found");
+//         return;
+//       }
+//     };
+  
+//     useEffect(() => {
+//         getAllTransactions();
+//     }, []);
+  
+//     return (
+//       <>
+//         <NavBar user={userDetails} />
+//         <section className="overflow-hidden main">
+//           <div className="container">
+//             <div className="row">
+//               <div className="col">
+//                 <table className="table table-bordered border-primary">
+//                   <thead>
+//                     <tr class="table-success">
+//                       <th scope="col">Serial No.</th>
+//                       <th scope="col">Trasaction Id</th>
+//                       <th scope="col">Transaction Type</th>
+//                       <th scope="col">Amount</th>
+//                       <th scope="col">Status</th>
+//                       <th scope="col">Sender Account Number</th>
+//                       <th scope="col">Receiver Account Number</th>
+//                       <th scope="col">Transaction date</th>
+//                     </tr>
+//                   </thead>
+//                   <tbody>
+//                     {transactions.map((transaction, index) => {
+//                       return (
+                        
+//                         <tr class="table-success">
+//                           <td>{index + 1}</td>
+//                           <td>{transaction.tid}</td>
+//                           <td>{transaction.transactionType}</td>
+//                           <td>{transaction.amount}</td>
+//                           <td>{transaction.status}</td>
+//                           <td>{transaction.senderAccNO}</td>
+//                           <td>{transaction.reciverAccNO}</td>
+//                           <td>{transaction.createdOn}</td>
+//                         </tr>
+//                       );
+//                     })}
+//                   </tbody>
+//                 </table>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+//       </>
+//     );
+// }
+
+// export default Passbook
